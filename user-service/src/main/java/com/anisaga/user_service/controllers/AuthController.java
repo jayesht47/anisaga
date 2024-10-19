922cf6e40c37f62a24e8ada1138297920574a3ed
Jayesh <jayeshthakur47@gmail.com>
2024-10-20 04:08:57 +0530

    added default user authority on register api

--- user-service/src/main/java/com/anisaga/user_service/controllers/AuthController.java
+++ user-service/src/main/java/com/anisaga/user_service/controllers/AuthController.java
@@ -16,12 +16,14 @@ import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -92,7 +94,8 @@ public class AuthController {
                     user.setAccountNonExpired(true);
                     user.setAccountNonLocked(true);
                     user.setCredentialsNonExpired(true);
-                    User resultUser = userService.upsertUser(user);
+                    user.setAuthorities(Collections.singleton(new SimpleGrantedAuthority("ROLE_USER")));
+                    userService.upsertUser(user);
                     String token = jwtUtil.generateToken(user);
                     responseMap.put("error", false);
                     responseMap.put("message", "User Registered Successfully");
