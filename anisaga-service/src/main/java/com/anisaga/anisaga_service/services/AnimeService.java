4558cc49bff9683e60b9dbb83467cb3424ec2c15
Jayesh Thakur <jayesh.thakur@cognizant.com>
2025-01-08 16:08:01 +0530

    refactored the code to create and move API calls to a separate service

---
+++ anisaga-service/src/main/java/com/anisaga/anisaga_service/services/AnimeService.java
@@ -0,0 +1,15 @@
+package com.anisaga.anisaga_service.services;
+
+import com.anisaga.anisaga_service.entities.Anime;
+import java.util.List;
+
+public interface AnimeService {
+
+    Anime getAnimeById(String animeId);
+
+
+    Anime getAnimeBySlug(String slug);
+
+
+    List<Anime> getTrendingAnime();
+}
